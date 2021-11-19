04e19114b3f9329ce94263cb9e3302c019f4cc59
ganlin <1316245375@qq.com>
2021-11-19 17:31:17 +0800

    v1

---
+++ HotItemsAnalysis/src/main/java/com/ganlin/hotitems_analysis/KafkaProducerUtil.java
@@ -0,0 +1,35 @@
+package com.ganlin.hotitems_analysis;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Properties;
+
+public class KafkaProducerUtil {
+    public static void main(String[] args) throws Exception {
+        writeToKafka("hotitems");
+    }
+    //包装一个写入kafka的方法
+    public static void writeToKafka(String topic) throws Exception{
+        //kafka配置
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","localhost:9092");
+        properties.setProperty("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        properties.setProperty("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+
+        //定义一个kafka producer
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+
+        //用缓冲方式读取文本
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("D:\\ganlin10\\Desktop\\waterdrop\\UserBehaviorAnalysis\\HotItemsAnalysis\\src\\main\\resources\\UserBehavior.csv"));
+        String line;
+        while((line = bufferedReader.readLine())!=null){
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, line);
+            //用producer发送数据
+            kafkaProducer.send(producerRecord);
+        }
+        kafkaProducer.close();
+    }
+}
