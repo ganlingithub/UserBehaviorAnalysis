b9563f391656b6fb156ec959c5d10576321802d9
ganlin <1316245375@qq.com>
2021-11-25 18:47:29 +0800

    v1

--- LoginFailDetect/src/main/java/com/ganlin/loginfail_detect/LoginFailWithCep.java
+++ LoginFailDetect/src/main/java/com/ganlin/loginfail_detect/LoginFailWithCep.java
@@ -2,29 +2,19 @@ package com.ganlin.loginfail_detect;
 
 import com.ganlin.loginfail_detect.beans.LoginEvent;
 import com.ganlin.loginfail_detect.beans.LoginFailWarning;
-import org.apache.flink.api.common.state.ListState;
-import org.apache.flink.api.common.state.ListStateDescriptor;
-import org.apache.flink.api.common.state.ValueState;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.cep.CEP;
 import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
 import org.apache.flink.cep.pattern.Pattern;
 import org.apache.flink.cep.pattern.conditions.SimpleCondition;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.shaded.guava18.com.google.common.collect.Lists;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.util.Collector;
-import org.apache.kafka.common.security.auth.Login;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -35,7 +25,7 @@ public class LoginFailWithCep {
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         //1.从文件中读取数据
         URL resource = LoginFailWithCep.class.getResource("/LoginLog.csv");
-        SingleOutputStreamOperator<LoginEvent> loginEventStream = env.readTextFile(resource.getPath())
+        DataStream<LoginEvent> loginEventStream = env.readTextFile(resource.getPath())
                 .map(line -> {
                             String[] fields = line.split(",");
                             return new LoginEvent(
@@ -56,7 +46,7 @@ public class LoginFailWithCep {
                 );
         //1.定义一个匹配模式
         //firstFail - > secondFail,within 2s
-        Pattern<LoginEvent, LoginEvent> loginFailPattern = Pattern.<LoginEvent>begin("firstFail").where(
+        Pattern<LoginEvent, LoginEvent> loginFailPattern1 = Pattern.<LoginEvent>begin("firstFail").where(
                 new SimpleCondition<LoginEvent>() {
                     @Override
                     public boolean filter(LoginEvent loginEvent) throws Exception {
@@ -72,7 +62,20 @@ public class LoginFailWithCep {
                             }
                         }
                 )
-                .within(Time.seconds(2));
+                .within(Time.seconds(3));
+
+        Pattern<LoginEvent, LoginEvent> loginFailPattern = Pattern
+                .<LoginEvent>begin("failEvents").where(
+                        //(SimpleCondition) (val) ->{ "fail".equals(val.getLoginState()); }
+                        new SimpleCondition<LoginEvent>() {
+                            @Override
+                            public boolean filter(LoginEvent loginEvent) throws Exception {
+                                return "fail".equals(loginEvent.getLoginState());
+                            }
+                        }
+                )
+                .times(3).consecutive()
+                .within(Time.seconds(5));
 
         //2.将匹配模式应用到数据流上，得到一个pattern stream
         PatternStream<LoginEvent> patternStream = CEP.pattern(loginEventStream.keyBy(LoginEvent::getUserId), loginFailPattern);
@@ -85,6 +88,19 @@ public class LoginFailWithCep {
     }
     //实现自定义PatternSelectFunction
     public static class LoginFailMatchDetecWarning implements PatternSelectFunction<LoginEvent,LoginFailWarning> {
+        @Override
+        public LoginFailWarning select(Map<String, List<LoginEvent>> map) throws Exception {
+            LoginEvent firstFailEvent = map.get("failEvents").get(0);
+            LoginEvent lastFailEvent = map.get("failEvents").get(map.get("failEvents").size()-1);
+            return new LoginFailWarning(
+                    firstFailEvent.getUserId(),
+                    firstFailEvent.getTimestamp(),
+                    lastFailEvent.getTimestamp(),
+                    "login fail "+map.get("failEvents").size()+" times"
+            );
+        }
+    }
+    public static class LoginFailMatchDetecWarning1 implements PatternSelectFunction<LoginEvent,LoginFailWarning> {
         @Override
         public LoginFailWarning select(Map<String, List<LoginEvent>> map) throws Exception {
             LoginEvent firstFailEvent = map.get("firstFail").iterator().next();
